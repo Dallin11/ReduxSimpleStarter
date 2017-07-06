cd76d04b617b8616575001721c4c8b5d9d5ebee1
Your Name <you@example.com>
2017-07-06 00:58:21 -0600

    Learned about state, props and calling apis in react

---
+++ src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React,  { Component } from 'react';  //alwasy need to import react even fi you arent using it. 
+
+class SearchBar extends Component {
+   constructor(props) {
+       super(props);
+
+       this.state = { term: ''};
+   }
+   //whenever we use state we use class based components
+   //When data is changing over time thats when you would use state. 
+   
+    render() {
+        return (
+            <div>
+        <input 
+        value = {this.state.term}
+        onChange={event =>  this.setState({ term: event.target.value })} />
+            </div>
+            //Just to reference state not to define it reference is ok to use this.state^
+    ); 
+    }
+    }
+//Each class has its own state, when something is changed the render component reruns.
+//Functional components do not have state only class based components have state.
+// Only inside the constructor function do we define state. No where else... ever. 
+//Use this.setState everywhere else to update the state at different points in your app.
+
+
+export default SearchBar; 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDbzPONUOpwmt6n-5VN6Q3BREpw5UoMBrE';
+//Downwards data flow the most parent component is responsible for fetching data.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+// Create new component. This component should produce some html
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.setState({ videos });
+    //this.setState({ videos: videos });
+});
+    }
+//whenever we change a component from a function based component to a class based component, we need to go through and update all the refrences to props as this.props
+
+    render() {
+return (
+<div>
+    <SearchBar />
+    <VideoList videos={this.state.videos} />
+</div>
+);
+}
+}
+
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ ReactDOM.render(<App/>, document.querySelector('.container'));
